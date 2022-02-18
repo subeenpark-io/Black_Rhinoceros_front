e6d1f6d675074549e0a6d48141e3ceb80da0e992
halfkiwi90 <happykiwi90@gmail.com>
2022-02-18 17:33:20 +0900

    연결시 포트 색 관련 useEffect 개선

--- src/app/pages/FlowPage/index.tsx
+++ src/app/pages/FlowPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, DragEvent, useEffect } from 'react';
+import React, { useState, DragEvent, useEffect, useCallback } from 'react';
 import ReactFlow, {
   ReactFlowProvider,
   addEdge,
@@ -33,20 +33,54 @@ const getId = (): ElementId => `dndnode_${id++}`;
 const FlowPage = () => {
   const [reactFlowInstance, setReactFlowInstance] = useState<OnLoadParams>();
   const [elements, setElements] = useState<Elements>(initialElements);
+  const [params, setParams] = useState<Edge | Connection | null>(null);
 
   const onElementsRemove = (elementsToRemove: Elements) =>
     setElements(els => removeElements(elementsToRemove, els));
   const onLoad = (_reactFlowInstance: OnLoadParams) =>
     setReactFlowInstance(_reactFlowInstance);
 
-  useEffect(() => {
-    const nodes = elements.filter(el => isNode(el)) as Node[];
-    const edges = elements.filter(el => isEdge(el)) as Edge[];
+  const onConnect = useCallback(
+    (params: Edge | Connection): Elements => {
+      const nodes = elements.filter(ele => isNode(ele)) as Node[];
+      const edges = elements.filter(ele => isEdge(ele)) as Edge[];
 
+      // 연결하려고 하는 포트의 엣지가 존재하면 그 엣지 삭제
+      const paramsSourceId = (params.source as string) + params.sourceHandle;
+      const paramsTargetId = (params.target as string) + params.targetHandle;
+
+      const removedEdges = edges.filter(edge => {
+        if (edge.source + edge.sourceHandle === paramsSourceId) {
+          return false;
+        }
+        if (edge.target + edge.targetHandle === paramsTargetId) {
+          return false;
+        }
+        return true;
+      });
+
+      // 엣지는 params를 수청해서 커스텀한다.
+      const customizedParams = {
+        ...params,
+        type: 'custom',
+        arrowHeadType: ArrowHeadType.ArrowClosed,
+      };
+      return addEdge(customizedParams, [...nodes, ...removedEdges]);
+    },
+    [params],
+  );
+
+  useEffect(() => {
+    const newElements = params ? onConnect(params) : elements;
+    const nodes = newElements.filter(ele => isNode(ele)) as Node[];
+    const edges = newElements.filter(ele => isEdge(ele)) as Edge[];
+
+    // data의 scource, targe을 초기화 해서 모든 노드의 포트를 빈 포트로 만든다(색을 지운다).
     const resetNodes = nodes.map(node => {
       return { ...node, data: { ...node.data, source: [], target: [] } };
     });
 
+    // 노드들과 엣지들을 모두 순회해서 포트들의 색을 다시 칠한다.
     const newNodes = resetNodes.map(node => {
       edges.forEach(edge => {
         if (node.id === edge.source) {
@@ -64,37 +98,9 @@ const FlowPage = () => {
       return node;
     });
 
-    setElements(els => [...newNodes, ...edges]);
-    // 일단 노드, 엣지가 추가되거나 삭제되면 실행하게끔 엘리먼츠 길이를 디펜던시로 잡았다.
-    // 길이가 변하지 않는 엘리먼츠 변화가 있을 수 있다. (ex 엣지 업데이트)
-    // 추후에 관련 기능이 추가되면 수정해야 할듯.
-  }, [elements.length]);
+    setElements([...newNodes, ...edges]);
+  }, [onConnect]);
 
-  const onConnect = async (params: Edge | Connection) => {
-    // 연결하려고 하는 포트의 엣지가 존제하면 그 엣지 삭제
-    const edges = elements.filter(ele => isEdge(ele)) as Edge[];
-    const paramsSourceId = (params.source as string) + params.sourceHandle;
-    const paramsTargetId = (params.target as string) + params.targetHandle;
-
-    // await 전구가 뜨지만 작동은 잘되는데,,,
-    // 삭제를 하고, 추가하는 순서
-    await edges.forEach(edge => {
-      if (edge.source + edge.sourceHandle === paramsSourceId) {
-        setElements(el => removeElements([edge], el));
-      }
-      if (edge.target + edge.targetHandle === paramsTargetId) {
-        setElements(el => removeElements([edge], el));
-      }
-    });
-
-    // 엣지는 params를 수청해서 커스텀한다.
-    const customizedParams = {
-      ...params,
-      type: 'custom',
-      arrowHeadType: ArrowHeadType.ArrowClosed,
-    };
-    setElements(els => addEdge(customizedParams, els));
-  };
   const onDrop = (event: DragEvent) => {
     event.preventDefault();
 
@@ -122,7 +128,9 @@ const FlowPage = () => {
         <Wrappper>
           <ReactFlow
             elements={elements}
-            onConnect={onConnect}
+            onConnect={connection => {
+              setParams(connection);
+            }}
             onElementsRemove={onElementsRemove}
             onLoad={onLoad}
             onDrop={onDrop}
