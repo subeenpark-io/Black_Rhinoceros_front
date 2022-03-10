5b856ef884f8077fe5f50f64820981cdb3f24c3c
halfkiwi90 <happykiwi90@gmail.com>
2022-03-10 18:09:34 +0900

    app global state 추가

--- src/types/RootState.ts
+++ src/types/RootState.ts
@@ -1,5 +1,6 @@
 // [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
 import { DatapageState } from 'app/pages/DataPage/slice/types';
+import { GlobalState } from 'app/pages/App/slice/types';
 
 /* 
   Because the redux-injectors injects your reducers asynchronously somewhere in your code
@@ -8,4 +9,5 @@ import { DatapageState } from 'app/pages/DataPage/slice/types';
 export interface RootState {
   // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
   datapage: DatapageState;
+  global: GlobalState;
 }
